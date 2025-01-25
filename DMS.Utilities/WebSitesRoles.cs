4aee4eee640fbc9b9a809bc18cd87840a302c6b4
nenadgolubovic <nenad.golubovic999@gmail.com>
2025-01-25 16:39:00 +0100

    Add Roles in utilities. Add identity.

---
+++ DMS.Utilities/WebSitesRoles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMS.Utilities
+{
+    public class WebSitesRoles
+    {
+        public const string WebSiteAdmin = "admin";
+        public const string WebSiteAssetManager = "assetManager";
+        public const string WebSiteCustomer = "customer";
+    }
+}
--- DocumentationManagementSystem/Program.cs
+++ DocumentationManagementSystem/Program.cs
@@ -1,5 +1,6 @@
 using DMS.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
