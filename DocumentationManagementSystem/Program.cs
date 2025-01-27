9d800af2d5d5d045325337693653881c4a83e760
nenadgolubovic <nenad.golubovic999@gmail.com>
2025-01-27 11:15:17 +0100

    Moved from AddDefaultIdentity to AddIdentity to support roles and advanced configurations

--- DocumentationManagementSystem/Program.cs
+++ DocumentationManagementSystem/Program.cs
@@ -14,7 +14,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser,IdentityRole>().
+    AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
