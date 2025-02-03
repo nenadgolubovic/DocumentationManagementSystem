44fba825641f5adb3218d22044bc2efdd77f3abc
nenadgolubovic <nenad.golubovic999@gmail.com>
2025-02-03 18:02:58 +0100

    Add connection between Component Sheet on page and base

---
+++ DMS.Repositories/Migrations/20250203163725_AddCoreTables.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DMS.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCoreTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Component",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Component", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Document",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Document", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Engine",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Manufacturer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Owner = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CSN = table.Column<int>(type: "int", nullable: false),
+                    TSN = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Engine", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Supplier",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Contact = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BillNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Supplier", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vendor",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Contact = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BillNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vendor", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Component");
+
+            migrationBuilder.DropTable(
+                name: "Document");
+
+            migrationBuilder.DropTable(
+                name: "Engine");
+
+            migrationBuilder.DropTable(
+                name: "Supplier");
+
+            migrationBuilder.DropTable(
+                name: "Vendor");
+        }
+    }
+}
--- DocumentationManagementSystem/Controllers/ComponentController.cs
+++ DocumentationManagementSystem/Controllers/ComponentController.cs
@@ -1,12 +1,21 @@
 ﻿using DMS.Models;
+using DMS.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using SQLitePCL;
 using System.IO;
+using DMS.Repositories;
+using System;
 
 namespace DocumentationManagementSystem.Controllers
 {
     public class ComponentController : Controller
     {
-        private static List<Component> component = new List<Component>();
+        private readonly ApplicationDbContext _context;
+        public ComponentController(ApplicationDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -17,18 +26,26 @@ namespace DocumentationManagementSystem.Controllers
         }
         public IActionResult Component()
         {
-            return View(component);
+            var components = _context.Component.ToList(); 
+            return View(components);
         }
+
 
 
         [HttpPost]
 
-        public IActionResult AddComponent(int id, string desctiption, string name, string type) 
+        public IActionResult AddComponent(ComponentViewModel model) 
         {
-            if (!string.IsNullOrEmpty(name))
+            if (!string.IsNullOrEmpty(model.Name))
             {
-                var newComponent = new Component { Id = component.Count + 1, Name = name, Description = desctiption, Type = type };
-                component.Add(newComponent);
+                var newComponent = new Component
+                {
+                    Name = model.Name,
+                    Description = model.Description,
+                    Type = model.Type
+                };
+                _context.Component.Add(newComponent);
+                _context.SaveChanges();
             }
             return RedirectToAction("Component");
         }
